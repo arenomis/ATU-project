18890e09f3a97e7e32fd826978c5ab5c73bea796
arenomis <136099080+arenomis@users.noreply.github.com>
2023-08-29 19:18:13 +0500

    Update scriptReg.js

--- reg/scriptReg.js
+++ reg/scriptReg.js
@@ -1,7 +1,7 @@
 var passwordReg = new Vue({
   el: '#LoginForm',
   data: {
-    username: '',
+    /*productionTip: false, убрать комент при публикации*/
     email: '',
     passwordUs: '',
     passwordUs2: '',
@@ -14,16 +14,11 @@ var passwordReg = new Vue({
     showPassword2: false,
     emailAlreadyRegistered: false,
     phone: '7',
-    INN: ''
+    showErrorPhone: false, // Флаг для отображения ошибки в номере телефона
+    INN: '',
+    formSubmitted: false, // Флаг для отслеживания отправки формы
   },
   computed: {
-    isValidPassword() {
-      var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]+$/;
-      return regex.test(this.passwordUs); 
-    }
-    
-  },
-  methods: {
     formatINN() {
       // Удаляем все, кроме цифр
       this.INN = this.INN.replace(/\D/g, '');
@@ -33,22 +28,37 @@ var passwordReg = new Vue({
         this.INN = this.INN.slice(0, 10);
       }
     },
+    isValidPassword() {
+      var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]+$/;
+      return regex.test(this.passwordUs); 
+    }
+  },
+  methods: {
     formatPhoneNumber() {
       // Проверяем и обрезаем номер телефона, чтобы он всегда начинался с '+7'
+      this.phone = this.phone.replace(/\D/g, '');
+
       if (!this.phone.startsWith('7')) {
           this.phone = '7' + this.phone;
       }
-      
-  },
-  toggleShowPassword2() {
-    this.showPassword2 = !this.showPassword2;
-    this.showPassword = !this.showPassword;
-},
-    RegUser: function(event) {
+      if (this.formSubmitted && this.phone.length !== 11) { 
+        this.showErrorPhone = true;
+      } else {
+        this.showErrorPhone = false;
+        this.formSubmitted = false;
+      }
+    },
+    toggleShowPassword2() {
+      this.showPassword2 = !this.showPassword2;
+      this.showPassword = !this.showPassword;
+    },
+    RegUser(event) {
       event.preventDefault(); // Отменяем стандартное действие отправки формы
 
+      this.formSubmitted = true; // Устанавливаем флаг, что форма была отправлена
+
       // Проверяем на незаполненные поля
-      if (!this.email.trim() || !this.passwordUs.trim() || !this.passwordUs2.trim() || !this.name.trim() || !this.lastname.trim() || !this.surname.trim()) {
+      if (!this.email.trim() || this.INN.trim() || !this.passwordUs.trim() || !this.passwordUs2.trim() || !this.name.trim() || !this.lastname.trim() || !this.surname.trim()) {
         this.showError = true;
         return;
       }
@@ -57,9 +67,12 @@ var passwordReg = new Vue({
         this.showError = true;
         return;
       }
+      if (this.phone.length !== 11) {
+          this.showErrorPhone = true;
+        return;
+      }
 
-      axios.post('#', {//Адрес сервера сюда как будет 
-        // передаем данные пользователя
+      axios.post('#', { // Адрес сервера сюда как будет передаем данные пользователя
 
       })
       .then(response => {
@@ -69,23 +82,24 @@ var passwordReg = new Vue({
         } else {
           // Дополнительный код для обработки успешной регистрации
         }
+        this.showErrorPhone = false;
       })
       .catch(error => {
         // Обработка ошибок
         console.error(error);
       });
-   
-      
 
       // Сброс ошибок и полей после сохранения
       this.showError = false;
-      this.username = '';
+      this.showErrorPhone = false;
       this.email = '';
+      this.phone = '7';
       this.passwordUs = '';
       this.passwordUs2 = '';
       this.name = '';
       this.lastname = '';
       this.surname = '';
+      this.inn = '';
       this.position = 'employee';
     }
   }
